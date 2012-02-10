980ff501166c0578d56c3aeddd6de46cc20d5469
Jakub Korab <jakub.korab@gmail.com>
2012-02-10 15:46:39 +0000

    Adding licences and copyrights

--- dm-integration-testing/dm-bundle/src/test/java/com/fusesource/examples/dm_bundle/CamelMockedRouteTest.java
+++ dm-integration-testing/dm-bundle/src/test/java/com/fusesource/examples/dm_bundle/CamelMockedRouteTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 FuseSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fusesource.examples.dm_bundle;
 
 import java.util.concurrent.TimeUnit;
@@ -11,6 +26,11 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * Camel route unit test.
+ * 
+ * @author jkorab
+ */
 public class CamelMockedRouteTest extends CamelSpringTestSupport {
 
 	@EndpointInject(uri = "activemq:in")
